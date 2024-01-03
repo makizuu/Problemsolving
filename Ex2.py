b6eaaffd8f0d35882ad3c3141c9873cb583a1507
Marika <marika20004@student.hamk.fi>
2024-01-03 12:50:40 +0200

    Exercise2 changes

--- Ex2.py
+++ Ex2.py
@@ -2,10 +2,10 @@ num = int(input("Give a number: "))
 check = int(input("Give a number to divide with: "))
 y = num % 2
 if num % check == 0:
-    print("First number is evenly dividable by the other number")
+    print(num, "is evenly dividable by", check)
 elif num % 4 == 0:
-    print("First number is a multiple of 4")
+    print(num, "is a multiple of 4")
 elif y == 0:
-    print("First number is an even number")
+    print(num, "is an even number")
 else:
-    print("First number is an odd number")
+    print(num, "is an odd number")
