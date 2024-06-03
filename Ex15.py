4d3ac6900485d19f6068406d5ccc4edd8c490111
Marika Tamminen <marika.tamminen@student.hamk.fi>
2024-06-03 19:13:48 +0300

    Cleanup

--- Ex15.py
+++
@@ -1,8 +0,0 @@
-#gives the user given string in backwards order
-def reverse(phrase):
-    words = phrase.split()
-    words.reverse()
-    return ' '.join(words)
-
-a = input("Give me a phrase: ")
-print(reverse(a))
